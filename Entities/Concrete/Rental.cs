2ec1fa84d187e888bd7a5743158efdd982523ebe
Burcu Taş <99994420+burcutas96@users.noreply.github.com>
2022-04-08 13:36:23 +0300

    Update Rental.cs

--- Entities/Concrete/Rental.cs
+++ Entities/Concrete/Rental.cs
@@ -14,6 +14,5 @@ namespace Entities.Concrete
         public int CustomerId { get; set; }
         public DateTime RentDate { get; set; }
         public DateTime? ReturnDate { get; set; }
-
     }
 }
