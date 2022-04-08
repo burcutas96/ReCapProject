e328350fc0aed83cddaacb21855c2ff34dbb3437
Burcu Taş <99994420+burcutas96@users.noreply.github.com>
2022-04-08 13:36:46 +0300

    Update User.cs

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -14,5 +14,6 @@ namespace Entities.Concrete
         public string LastName { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
+        
     }
 }
